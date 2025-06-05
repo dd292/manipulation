5a6fb8d7357e7af32a88217f767a47dc0591fb30
Daksh Dhingra <dakshdhingra93@gmail.com>
2025-06-05 02:22:13 -0500

    wokring jacobian, position command:

--- dual_franka_model/src/mujoco_node.cpp
+++ dual_franka_model/src/mujoco_node.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include "ament_index_cpp/get_package_share_directory.hpp"
 #include <Eigen/Dense>
+#include <cmath>
 
 using namespace std::chrono_literals;
 
@@ -68,10 +69,12 @@ class MujocoSimNode : public rclcpp::Node {
     
             // ROS publisher
             joint_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_state", 10);
-    
+            // end_effector_pos = this->create_publisher<geometry_msgs::msg::Point>("end_effector_pos", 3);
+            // end_effector_quat = this->create_publisher<geometry_msgs::msg::Quaternion>("end_effector_quat", 4);
             // Create a timer loop
             sim_timer_ = this->create_wall_timer(10ms, std::bind(&MujocoSimNode::step_simulation, this));
             RCLCPP_INFO(this->get_logger(), "Mujoco sim node started");
+            
         }
     
         ~MujocoSimNode() {
@@ -91,16 +94,6 @@ class MujocoSimNode : public rclcpp::Node {
                 return;
             }
             
-    
-            // Publish joint states
-            auto msg = sensor_msgs::msg::JointState();
-            msg.header.stamp = this->get_clock()->now();
-            for (int i = 0; i < model_->nq; i++) { // number of generalized coordinates
-                msg.name.push_back(model_->names + model_->name_jntadr[i]);
-                msg.position.push_back(data_->qpos[i]);
-            }
-            joint_pub_->publish(msg);
-    
             // Render the scene
             int width, height;
             glfwGetFramebufferSize(window_, &width, &height);
@@ -109,29 +102,79 @@ class MujocoSimNode : public rclcpp::Node {
             mjr_render(viewport, &scene_, &context_);
             glfwSwapBuffers(window_);
             glfwPollEvents();
-            
-            // for (int i = 0; i < model_->nu; ++i)
-            //     data_->ctrl[i] =1.0;
+
             int banana_id = mj_name2id(model_, mjOBJ_BODY, "banana");
             const mjtNum* banana_pos = data_->xpos + 3 * banana_id;
-            
-            
+
+            //Get banan and gripper positions
+            int gripper_id = mj_name2id(model_, mjOBJ_BODY, "hand");
+            const mjtNum* gripper_pos = data_->xpos + 3 * gripper_id;
+           
+           
+            //COmpute IK to move gripper closer to banana
+            Eigen::Vector3d banana_pos_eigen(banana_pos[0], banana_pos[1], banana_pos[2]);
+            Eigen::Vector3d gripper_pos_eigen(gripper_pos[0], gripper_pos[1], gripper_pos[2]);
+            Eigen::Vector3d delta_pos = banana_pos_eigen - gripper_pos_eigen;
+
+            double threshold = 0.001;
+            int max_iterations = 100;
+            int iteration = 0;
+            if (!sim_initialized_)
+            {
+                // Initialize the simulation
+                mj_resetData(model_, data_);
+                mj_step(model_, data_);
+                sim_initialized_ = true;
+            }
+            while (delta_pos.norm()> threshold && iteration <max_iterations)
+            {
+
+                //compute the Jacobian
+                Eigen::MatrixXd jacobian(3, model_->nu);
+                mj_jac(model_, data_, jacobian.data(), nullptr, gripper_pos, gripper_id);
+
+                
+                // Compute joint velocity using pseudo-inverse
+                Eigen::VectorXd joint_velocity = jacobian.completeOrthogonalDecomposition().solve(delta_pos);
+
+                for (int i = 0; i < model_->nu; ++i){
+                    data_->ctrl[i]= data_->qpos[i]+ joint_velocity[i]* model_->opt.timestep; // Apply the joint velocity to the control input
+                }
     
-            // Step the simulation
-            mj_step(model_, data_);
+                // Step the simulation
+                mj_step(model_, data_);
+                std::cerr << "Simulation time: " << data_->time << std::endl;
+        
+                gripper_pos_eigen = Eigen::Vector3d(data_->xpos[3 * gripper_id], data_->xpos[3 * gripper_id + 1], data_->xpos[3 * gripper_id + 2]);
+                delta_pos = banana_pos_eigen - gripper_pos_eigen;
+                iteration++;
+            }
+            
+            // Publish joint states
+            auto msg = sensor_msgs::msg::JointState();
+            msg.header.stamp = this->get_clock()->now();
+            for (int i = 0; i < model_->nq; i++) { // number of generalized coordinates
+                msg.name.push_back(model_->names + model_->name_jntadr[i]);
+                msg.position.push_back(data_->qpos[i]);
+            }
+            joint_pub_->publish(msg);
+            
+
         }
     
         mjModel* model_ = nullptr;
         mjData* data_ = nullptr;
         rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_pub_;
+        // rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr end_effector_pos;
+        // rclcpp::Publisher<geometry_msgs::msg::Quaternion>::SharedPtr end_effector_quat;
         rclcpp::TimerBase::SharedPtr sim_timer_;
-    
         // Visualization components
         GLFWwindow* window_ = nullptr;
         mjvCamera cam_;
         mjvOption opt_;
         mjvScene scene_;
         mjrContext context_;
+        bool sim_initialized_= false; // Flag to check if simulation is initialized
 };
 
 int main(int argc, char* argv[]) {
